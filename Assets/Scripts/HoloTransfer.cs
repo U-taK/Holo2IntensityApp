719532165a6f2e42358b8f9315d03c01cb44ec08
U-taK <9882bluebird@gmail.com>
2020-07-24 20:10:16 +0900

    add uOSC scene

---
+++ Assets/Scripts/HoloTransfer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace uOSC
+{
+    public class HoloTransfer : MonoBehaviour
+    {
+        public TextMesh text;
+        // Start is called before the first frame update
+        void Start()
+        {
+            var server = GetComponent<uOscServer>();
+            server.onDataReceived.AddListener(OnDataReceived);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var client = GetComponent<uOscClient>();
+            client.Send("/uOSC/test", "send HMD message to PC");
+        }
+
+        void OnDataReceived(Message message)
+        {
+            // address
+            var msg = message.address + ": ";
+
+            // timestamp
+            msg += "(" + message.timestamp.ToLocalTime() + ") ";
+
+            // values
+            foreach (var value in message.values)
+            {
+                msg += value.GetString() + " ";
+            }
+
+            text.text = msg;
+        }
+    }
+}
---
+++ Assets/Scripts/PCTransfer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace uOSC
+{
+    public class PCTransfer : MonoBehaviour
+    {
+        int i = 0;
+        // Start is called before the first frame update
+        void Start()
+        {
+            var server = GetComponent<uOscServer>();
+            server.onDataReceived.AddListener(OnDataReceived);
+           
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var client = GetComponent<uOscClient>();
+            client.Send("/uOSC/test", "send PC message for HMD"+i);
+            i++;
+        }
+
+        void OnDataReceived(Message message)
+        {
+            // address
+            var msg = message.address + ": ";
+
+            // timestamp
+            msg += "(" + message.timestamp.ToLocalTime() + ") ";
+
+            // values
+            foreach (var value in message.values)
+            {
+                msg += value.GetString() + " ";
+            }
+
+            Debug.Log(msg);
+
+        }
+    }
+}
