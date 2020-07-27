8b59970d6a6907153817e3a8155f14ef17f0a920
U-taK <9882bluebird@gmail.com>
2020-07-27 19:25:51 +0900

    add Main scene

---
+++ Assets/Scripts/VisualizationPart/MicPositionMirror.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicPositionMirror : MonoBehaviour
+{
+    [SerializeField]//マイクマーカによって取得する計測点位置
+    GameObject micPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = micPos.gameObject.transform.position;
+        this.transform.rotation = micPos.gameObject.transform.rotation;
+    }
+
+    //計測点の座標情報を逐次送信
+    public void GetSendInfo(out Vector3 vector, out Quaternion quaternion) 
+    {
+        vector = this.transform.localPosition;
+        quaternion = this.transform.localRotation;
+    }
+}
---
+++ Assets/Scripts/VisualizationPart/OriginChaser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginChaser : MonoBehaviour
+{
+    [SerializeField]
+    GameObject standardMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (standardMarker != null)
+        {
+            this.transform.position = standardMarker.transform.position;
+            this.transform.localRotation = standardMarker.transform.localRotation;
+        }
+    }
+}
