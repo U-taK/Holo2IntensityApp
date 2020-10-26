cf3eed13eb5a000ebca2b935aca4bb16fd061808
U-taK <9882bluebird@gmail.com>
2020-10-26 20:35:47 +0900

    FFTWの実装テスト

---
+++ Assets/Scripts/Measurement/FFTWTester.cs
@@ -0,0 +1,293 @@
+﻿using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using UnityEngine;
+
+public class FFTWTester : MonoBehaviour
+{
+    double[][] signal = new double[4][];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("length(4)");
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            //Test
+            double[] x = { 1.2, 3.4, 5.6, 7.8 };
+
+            var dft = MathFFTW.FFT(x, true);
+
+            var idft = MathFFTW.IFFT(dft);
+            sw.Stop();
+            Debug.Log("Test1: FFTW");
+            DisplayCompex(dft);
+            Debug.Log("IFFT = ");
+            DisplayReal(idft);
+            TimeSpan ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+
+            sw.Restart();
+            Complex[] cx = new Complex[4];
+            Complex[] cdft;
+            double[] cidft;
+            for (var i = 0; i < x.Length; i++)
+                cx[i] = new Complex(x[i], 0);
+
+
+            AcousticMathNew.FFT(2, cx, out cdft);
+            AcousticMathNew.IFFT(2, cdft, out cidft);
+            sw.Stop();
+            Debug.Log("Test2: Acoustic Math New");
+            DisplayCompex(dft);
+            Debug.Log("IFFT = ");
+            DisplayReal(idft);
+            ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("length(2048)");
+            ReadTestData();
+
+            var halfsig = new double[2048];
+            for (var i = 0; i < halfsig.Length; i++)
+                halfsig[i] = signal[0][i];
+
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            //Test
+
+            var dft = MathFFTW.FFT(halfsig, true);
+
+            var idft = MathFFTW.IFFT(dft);
+            sw.Stop();
+            Debug.Log("Test1: FFTW");
+            //DisplayCompex(dft);
+            //Debug.Log("IFFT = ");
+            //DisplayReal(idft);
+            TimeSpan ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+
+            sw.Restart();
+            Complex[] cx = new Complex[halfsig.Length];
+            Complex[] cdft;
+            double[] cidft;
+            for (var i = 0; i < halfsig.Length; i++)
+                cx[i] = new Complex(halfsig[i], 0);
+
+
+            AcousticMathNew.FFT(11, cx, out cdft);
+            AcousticMathNew.IFFT(11, cdft, out cidft);
+            sw.Stop();
+            Debug.Log("Test2: Acoustic Math New");
+            //DisplayCompex(dft);
+            //Debug.Log("IFFT = ");
+            //DisplayReal(idft);
+            ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("length(4096)");
+            ReadTestData();
+
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            //Test
+
+            var dft = MathFFTW.FFT(signal[0], true);
+
+            var idft = MathFFTW.IFFT(dft);
+            sw.Stop();
+            Debug.Log("Test1: FFTW");
+            //DisplayCompex(dft);
+            //Debug.Log("IFFT = ");
+            //DisplayReal(idft);
+            TimeSpan ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+
+            sw.Restart();
+            Complex[] cx = new Complex[signal[0].Length];
+            Complex[] cdft;
+            double[] cidft;
+            for (var i = 0; i < signal[0].Length; i++)
+                cx[i] = new Complex(signal[0][i], 0);
+
+
+            AcousticMathNew.FFT(12, cx, out cdft);
+            AcousticMathNew.IFFT(12, cdft, out cidft);
+            sw.Stop();
+            Debug.Log("Test2: Acoustic Math New");
+            //DisplayCompex(dft);
+            //Debug.Log("IFFT = ");
+            //DisplayReal(idft);
+            ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("SoundIntensity");
+            ReadTestData();
+
+            var sw = new System.Diagnostics.Stopwatch();
+            var fs = 44100;
+            var fmin = 707f;
+            var fmax = 1414f;
+            var adensity = 1.4f;
+            var dr = 0.05f;
+            sw.Start();
+            //処理
+            var intensity = MathFFTW.CrossSpectrumMethod(signal, fs, fmin, fmax, adensity, dr);
+            var level = MathFFTW.CalcuIntensityLevel(intensity);        
+            sw.Stop();
+            Debug.Log("Test1: FFTW");
+
+            Debug.Log($" Intensity:({intensity.x},{intensity.y},{intensity.z}) ");
+            Debug.Log($" Intensity level is {level}");
+
+            TimeSpan ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+
+            sw.Restart();
+            intensity = AcousticMathNew.CrossSpectrumMethod(signal, fs, 12, fmin, fmax, adensity, dr);
+            level = AcousticMathNew.CalcuIntensityLevel(intensity);
+            sw.Stop();
+            Debug.Log("Test2: Acoustic Math New");
+
+            Debug.Log($" Intensity:({intensity.x},{intensity.y},{intensity.z}) ");
+            Debug.Log($" Intensity level is {level}");
+
+            ts = sw.Elapsed;
+            Debug.Log($"　{sw.ElapsedMilliseconds}ミリ秒");
+        }
+    }
+
+    void DisplayCompex(double[] x)
+    {
+        if(x.Length % 2 != 0)
+        {
+            throw new Exception("The number of elements must be even");
+        }
+        for(int i = 0, n = x.Length; i < n; i+= 2)
+        {
+            if (x[i + 1] < 0)
+                Debug.Log(string.Format("{0} - {1}i", x[i], Math.Abs(x[i + 1])));
+            else
+                Debug.Log(string.Format("{0} + {1}i", x[i], Math.Abs(x[i + 1])));
+        }
+    }
+
+    void DisplayCompex(Complex[] x)
+    {      
+        for (int i = 0, n = x.Length; i < n; i++)
+        {                           
+            Debug.Log(string.Format("{0} + {1}i", x[i].Real, x[i + 1].Imaginary));
+        }
+    }
+
+
+    void DisplayReal(double[] x)
+    {
+        if (x.Length % 2 != 0)
+        {
+            throw new Exception("The number of elements must be even");
+        }
+        for (int i = 0, n = x.Length; i < n; i += 2)
+        {
+            Debug.Log(x[i]);
+        }
+    }
+
+    void ReadTestData()
+    {
+
+        //byteファイル読み込み
+
+        TextAsset asset1 = Resources.Load("TestData/mic1", typeof(TextAsset)) as TextAsset;
+        TextAsset asset2 = Resources.Load("TestData/mic2", typeof(TextAsset)) as TextAsset;
+        TextAsset asset3 = Resources.Load("TestData/mic3", typeof(TextAsset)) as TextAsset;
+        TextAsset asset4 = Resources.Load("TestData/mic4", typeof(TextAsset)) as TextAsset;
+        signal[0] = new double[asset1.bytes.Length / 8];
+        signal[1] = new double[asset2.bytes.Length / 8];
+        signal[2] = new double[asset3.bytes.Length / 8];
+        signal[3] = new double[asset4.bytes.Length / 8];
+
+        signal[0] = Bytes2array(asset1, asset1.bytes.Length / 8);
+        signal[1] = Bytes2array(asset2, asset2.bytes.Length / 8);
+        signal[2] = Bytes2array(asset3, asset3.bytes.Length / 8);
+        signal[3] = Bytes2array(asset4, asset4.bytes.Length / 8);
+    }
+
+
+    /// <summary>
+    /// bytesファイルをdouble型配列に
+    /// </summary>
+    /// <param name="asset">bytesファイル</param>
+    /// <param name="leng">読み込み長さ</param>
+    public static double[] Bytes2array(TextAsset asset, int leng)
+    {
+        double[] sound = new double[leng];
+        using (Stream fs = new MemoryStream(asset.bytes))
+        {
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                int i = 0;
+                while (i < leng)
+                {
+                    //符号付2byte読み込み
+                    //sound[i] = (double)br.ReadInt16();
+                    sound[i] = (double)br.ReadDouble();
+                    i++;
+                }
+            }
+        }
+        return sound;
+    }
+    /// <summary>
+    /// bytesファイルをshort型配列に
+    /// </summary>
+    /// <param name="asset">bytesファイル</param>
+    /// <param name="leng">読み込み長さ</param>
+    public static short[] Bytes2array_sh(TextAsset asset, int leng)
+    {
+        short[] sound = new short[leng];
+        using (Stream fs = new MemoryStream(asset.bytes))
+        {
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                int i = 0;
+                while (i < leng)
+                {
+                    sound[i] = br.ReadInt16();
+                    i++;
+                }
+            }
+        }
+        return sound;
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Measurement/MathFFTW.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Xml;
+using FFTWSharp;
+using UnityEngine;
+
+class MathFFTW
+{
+    /// <summary>
+    /// FFTWを使ったクロススペクトル法
+    /// FFTWの返り値の配列サイズは入力信号の倍になる(偶数が実数、奇数が虚数)
+    /// </summary>
+    /// <param name="sound_signals">入力信号(4ch)</param>
+    /// <param name="fs">サンプリング周波数//</param>
+    /// <param name="freq_range_min">周波数帯域下限</param>
+    /// <param name="freq_range_max">周波数帯域上限</param>
+    /// <param name="atmDensity">大気密度</param>
+    /// <param name="dr">マイク間距離</param>
+    /// <returns></returns>
+    public static Vector3 CrossSpectrumMethod(double[][] sound_signals, int fs, float freq_range_min, float freq_range_max, double atmDensity, float dr)
+    {
+        //サンプル長を求める
+        int sampleLength = sound_signals[0].Length;
+
+        double[][] fft = new double[4][];
+
+        for (int micID = 0; micID < 4; micID++)
+        {
+            //虚数を含めるから2倍の長さを確保
+            fft[micID] = new double[sampleLength*2];
+            fft[micID] = FFT(sound_signals[micID], true);
+            //FFT結果を平均化
+            for (int fftIndex = 0; fftIndex < fft[micID].Length; fftIndex++)
+                fft[micID][fftIndex] /= sampleLength;
+
+        }
+
+        //FFTした時のサンプル範囲を求める
+        float df = fs / sampleLength;
+        int fftIndexMin = Mathf.CeilToInt(freq_range_min / df)*2;
+        int fftIndexMax = Mathf.FloorToInt(freq_range_max / df)*2;
+
+        //インテンシティの積分範囲を求める
+        double sig01 = 0d;
+        double sig02 = 0d;
+        double sig03 = 0d;
+        double sig12 = 0d;
+        double sig13 = 0d;
+        double sig23 = 0d;
+        for (int fftIndex = fftIndexMin; fftIndex <= fftIndexMax; fftIndex+=2)
+        {
+            //ナイキスト周波数の時だけ計算(虚数がfftの結果に含まれるのでsampleLength*2/2)のイメージ)
+            if (fftIndex <= sampleLength)
+            {
+                //両側クロススペクトルの虚部
+                double imS01 = fft[0][fftIndex] * fft[1][fftIndex+1] - fft[1][fftIndex] * fft[0][fftIndex+1];
+                double imS02 = fft[0][fftIndex] * fft[2][fftIndex+1] - fft[2][fftIndex] * fft[0][fftIndex+1];
+                double imS03 = fft[0][fftIndex] * fft[3][fftIndex+1] - fft[3][fftIndex] * fft[0][fftIndex+1];
+                double imS12 = fft[1][fftIndex] * fft[2][fftIndex+1] - fft[2][fftIndex] * fft[1][fftIndex+1];
+                double imS13 = fft[1][fftIndex] * fft[3][fftIndex+1] - fft[3][fftIndex] * fft[1][fftIndex+1];
+                double imS23 = fft[2][fftIndex] * fft[3][fftIndex+1] - fft[3][fftIndex] * fft[2][fftIndex+1];
+
+                //両側クロススペクトル虚部 -> 片側クロススペクトル虚部
+                double imG01;
+                double imG02;
+                double imG03;
+                double imG12;
+                double imG13;
+                double imG23;
+                if (fftIndex == 0 || fftIndex == sampleLength)
+                {
+                    imG01 = imS01;
+                    imG02 = imS02;
+                    imG03 = imS03;
+                    imG12 = imS12;
+                    imG13 = imS13;
+                    imG23 = imS23;
+                }
+                else
+                {
+                    imG01 = 2 * imS01;
+                    imG02 = 2 * imS02;
+                    imG03 = 2 * imS03;
+                    imG12 = 2 * imS12;
+                    imG13 = 2 * imS13;
+                    imG23 = 2 * imS23;
+                }
+
+                //積分範囲
+                sig01 += imG01 / (fftIndex / 2);
+                sig02 += imG02 / (fftIndex / 2);
+                sig03 += imG03 / (fftIndex / 2);
+                sig12 += imG12 / (fftIndex / 2);
+                sig13 += imG13 / (fftIndex / 2);
+                sig23 += imG23 / (fftIndex / 2);
+            }
+        }
+
+        //インテンシティ計算
+        double i01 = sig01 / (2d * Math.PI * atmDensity * dr);
+        double i02 = sig02 / (2d * Math.PI * atmDensity * dr);
+        double i03 = sig03 / (2d * Math.PI * atmDensity * dr);
+        double i12 = sig12 / (2d * Math.PI * atmDensity * dr);
+        double i13 = sig13 / (2d * Math.PI * atmDensity * dr);
+        double i23 = sig23 / (2d * Math.PI * atmDensity * dr);
+
+        //左手系ワールド座標(x,y,z)に変換
+        double ix = -(i01 - i02 + i23 - i13 - (2 * i12)) / 4;
+        double iy = -(i01 + i02 + i03) / Math.Sqrt(6d);
+        double iz = -(-i01 - i02 + (2 * i03) + (3 * i13) + (3 * i23)) / (4d * Math.Sqrt(3d));
+        return new UnityEngine.Vector3((float)ix, (float)iy, (float)iz);
+    }
+
+    /// <summary>
+    /// 三次元音響インテンシティ(vec3)からインテンシティレベル(float)を計算
+    /// </summary>
+    public static float CalcuIntensityLevel(UnityEngine.Vector3 intensity)
+    {
+        float intensityNorm = intensity.magnitude;
+        float intensityLevel = 10f * Mathf.Log10(intensityNorm / Mathf.Pow(10f, -12f));
+        return intensityLevel;
+    }
+
+    /// <summary>
+    /// Computed fft of 1D array of real or complex numbers
+    /// </summary>
+    /// <param name="data">Input data</param>
+    /// <param name="real">Real or complex flag</param>
+    /// <returns>Return the fft.</returns>
+    public static double[] FFT(double[] data, bool real)
+    {
+        //if the input is real, make it complex
+        if (real)
+            data = ToComplex(data);
+        //Get the length of the array
+        int n = data.Length;
+
+        /* Allocate an unmanaged memory block for the input and output data.
+         * (The input and output are of the same length in this case, so we can use just one memory block.)*/
+        IntPtr ptr = fftw.malloc(n * sizeof(double)); //or n * sizeof(double)
+        //Pass the managed input data to the unmanged memory block
+        Marshal.Copy(data, 0, ptr, n);
+        //Plan the FFT and execute it (n/2 because complex numbers are stored as pairs of doubles)
+        IntPtr plan = fftw.dft_1d(n / 2, ptr, ptr, fftw_direction.Forward, fftw_flags.Estimate);
+        fftw.execute(plan);
+        //Create an array to store the output values
+        var fft = new double[n];
+        //Pass the unmanaged output data to the managed array
+        Marshal.Copy(ptr, fft, 0, n);
+        //Do some cleaning
+        fftw.destroy_plan(plan);
+        fftw.free(ptr);
+        fftw.cleanup();
+        //Return the FFT output
+        return fft;
+    }
+
+    /// <summary>
+    /// Computes the ifft of 1D array of complex numbers
+    /// </summary>
+    /// <param name="data">Input data.</param>
+    /// <returns>Returns the normalizedIFFT.</returns>
+    public static double[] IFFT(double[] data)
+    {      
+        //Get the length of the array
+        int n = data.Length;
+
+        /* Allocate an unmanaged memory block for the input and output data.
+         * (The input and output are of the same length in this case, so we can use just one memory block.)*/
+        IntPtr ptr = fftw.malloc(n * sizeof(double));
+        //Pass the managed input data to the unmanged memory block
+        Marshal.Copy(data, 0, ptr, n); //or n * sizeof(double)
+        //Plan the IFFT and execute it (n/2 because complex numbers are stored as pairs of doubles)
+        IntPtr plan = fftw.dft_1d(n / 2, ptr, ptr, fftw_direction.Backward, fftw_flags.Estimate);
+        fftw.execute(plan);
+        //Create an array to store the output values
+        var ifft = new double[n];
+        //Pass the unmanaged output data to the managed array
+        Marshal.Copy(ptr, ifft, 0, n);
+        //Do some cleaning
+        fftw.destroy_plan(plan);
+        fftw.free(ptr);
+        fftw.cleanup();
+        //Scale the output values
+        for (int i = 0, nh = n / 2; i < n; i++)
+            ifft[i] /= nh; 
+        //Return the IFFT output
+        return ifft;
+    }
+    /// <summary>
+    /// Interfaces an array with zeros to match the FFTW convention of representing complex numbers.
+    /// </summary>
+    /// <param name="real">An array of real numbers.</param>
+    /// <returns>Returns an array of complex numbers.</returns>
+    static double[] ToComplex(double[] real)
+    {
+        int n = real.Length;
+        var comp = new double[n * 2];
+        for (int i = 0; i < n; i++)
+            comp[2 * i] = real[i];
+        return comp;
+    }
+
+    /// <summary>
+    /// Original filter function(2次のバンドパスフィルタ)
+    /// 周波数を変更する場合はc_freqを変更
+    /// </summary>
+    /// <param name="input">入力信号</param>
+    /// <param name="signals">出力信号</param>
+    /// <param name="sampleLength">出力信号長</param>
+    public static void BPFilter(double[] input, out double[] signals, int sampleLength)
+    {
+        int c_freq = 1000;
+        float bw = 1f / 3f;
+        int fs = 44100;
+        float omega = 2 * Mathf.PI * c_freq / fs;
+        float alpha = Mathf.Sin(omega) * (float)Math.Sinh(Mathf.Log(2) / 2 * bw * omega / Math.Sin(omega));
+
+        float a0 = 1 + alpha;
+        float b0 = alpha / a0;
+        float b1 = 0;
+        float b2 = -alpha / a0;
+        float a1 = (-2 * Mathf.Cos(omega)) / a0;
+        float a2 = (1 - alpha) / a0;
+
+        //filter実装
+        signals = new double[sampleLength];
+        signals[0] = b0 * input[0];
+        signals[1] = b0 * input[1] + b1 * input[0] - a1 * signals[0];
+        for (int m = 2; m < sampleLength; m++)
+        {
+
+            //y[n] = b0 * x[n] + b1 * x[n - 1] + b2 * x[n - 2] - a1 * y[n - 1] - a2 * y[n - 2]
+
+            signals[m] = b0 * input[m] + b1 * input[m - 1] + b2 * input[m - 2] -
+            a1 * signals[m - 1] - a2 * signals[m - 2];
+        }
+    }
+    double RMS(double[] soundSignal)
+    {
+        int length = soundSignal.Length;
+        double rms = 0;
+        for (int s = 0; s < length; s++)
+        {
+            rms += Math.Pow(soundSignal[s], 2) / length;
+        }
+        Math.Sqrt(rms);
+        return rms;
+    }
+}
