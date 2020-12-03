ab07019859a07ac3440f113f6a8b1e38ae8899bc
U-taK <9882bluebird@gmail.com>
2020-12-03 17:34:25 +0900

    [HoloLens2]過渡計測の周波数応答変更に対応

--- Assets/Scripts/VisualizationPart/InstanceManager.cs
+++ Assets/Scripts/VisualizationPart/InstanceManager.cs
@@ -37,7 +37,7 @@ namespace uOSC
             VectorObj.name = "IntensityObject";
             var interact = VectorObj.GetComponent<Interactable>();
             if (interact != null)
-                interact.IsEnabled = !UIManager._measure;
+                interact.enabled = !UIManager._measure;
 
             var panel = VectorObj.GetComponentInChildren<IntensityPanel>();
             if (panel != null)
@@ -114,6 +114,37 @@ namespace uOSC
             }
         }
 
+        /// <summary>
+        /// 色変更
+        /// </summary>
+        /// <param name="No"></param>
+        /// <param name="newIntensity"></param>
+        /// <param name="color"></param>
+        public void ChangeInstantIntensityObj(int No, Vector3 newIntensity, Color color, Vector3[] iintensities)
+        {
+            if (intensities.ContainsKey(No))
+            {
+                var pushObj = intensities[No];
+                //色変更を行う
+                pushObj.transform.localRotation = Quaternion.LookRotation(10000000000 * newIntensity);
+                pushObj.transform.GetComponent<Renderer>().material.color = color;
+                
+                var storage = pushObj.GetComponent<IntensityObject>();
+                //瞬時音響インテンシティをリストに保持させる
+                List<Color> colors = new List<Color>();
+                List<Vector3> scales = new List<Vector3>();
+
+                foreach (var iintensiy in iintensities)
+                {
+                    var intensityLv = AIMath.CalcuIntensityLevel(iintensiy);
+                    colors.Add(ColorBar.DefineColor(Holo2MeasurementParameter.ColorMapID, intensityLv, Holo2MeasurementParameter.LevelMin, Holo2MeasurementParameter.LevelMax));
+                    scales.Add(DefineSize(intensityLv, Holo2MeasurementParameter.LevelMin, Holo2MeasurementParameter.LevelMax));
+                }
+                storage.PushSumIntensity(newIntensity, color);
+                storage.PushInstantIntensity(iintensities, colors, scales);
+            }
+        }
+
         //削除
         public void DeleteVectorObj(int dNum)
         {
@@ -178,7 +209,7 @@ namespace uOSC
             foreach(var intensity in intensities.Values)
             {
                 var interact = intensity.GetComponent<Interactable>();
-                interact.IsEnabled = !UIManager._measure;
+                interact.enabled = !UIManager._measure;
             }
         }
     }
